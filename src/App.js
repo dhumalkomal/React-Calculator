c64c0b23e8e9b815a33d97633f439b6364099232
dhumalkomal <107318301+dhumalkomal@users.noreply.github.com>
2023-12-30 14:24:40 +0530

    complete project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React, { Component } from 'react';
+import  KeyPadComponent  from './components/KeyPadComponent.js';
+import ResultComponent from './components/ResultComponent.js';
+// import './index.css';
+
+class App extends Component {
+  state = {
+    result: ""
+  }
+
+  onClick = button => {
+    if(button === '=') {
+      this.calculate();
+    }
+
+    else if(button === 'AC') {
+      this.reset();
+    }
+
+    else if(button === 'Delete') {
+      this.backspace();
+    }
+
+    else {
+      this.setState({
+        result: this.state.result + button
+      })
+    }
+  };
+
+  calculate = () => {
+    var checkResult = ''
+    if(this.state.result.includes('--')) {
+      checkResult = this.state.result.replace('--', '+')
+    }
+    else {
+      checkResult = this.state.result;
+    }
+
+    try {
+      this.setState({
+        result: (eval(checkResult) || "") + ""
+      })
+    }
+    catch(e) {
+      this.setState({
+        result: "error"
+      })
+    }
+  };
+
+  reset = () => {
+    this.setState({
+      result: ""
+    })
+  };
+
+  backspace = () => {
+    this.setState({
+      result: this.state.result.slice(0, -1)
+    })
+  };
+
+  render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="calculator-body">
+      <h1>Calculator App</h1>
+      <ResultComponent result={this.state.result}  />
+      <KeyPadComponent onClick={this.onClick} />
+      
     </div>
   );
+}
 }
 
 export default App;
